8a0577b2caa0cb41b4b9afbada3697beef349e35
Simone Gasparini <simone.gasparini.2@gmail.com>
2025-12-29 16:11:10 +0100

    Add "add" command with commander

--- expense-tracker.js
+++ expense-tracker.js
@@ -1,10 +1,31 @@
 #!/usr/bin/env node
 
 import fs from "fs";
+import { Command } from 'commander';
 
 let expenses = [];
+const program = new Command();
+
+program
+    .name("expense-tracker")
+    .version("1.0.0");
+
+program
+    .command("add")
+    .requiredOption("--description <description>")
+    .requiredOption("--amount <amount>")
+    .action((options) => {
+        let expense = {
+            description: options.description,
+            amount: options.amount,
+        };
+        expenses.push(expense);
+    });
 
 loadExpenses();
+program.parse();
+saveExpenses();
+
 
 function loadExpenses() {
     if (!fs.existsSync("expenses.json")) {
@@ -13,4 +34,9 @@ function loadExpenses() {
         const expensesJSON = fs.readFileSync("expenses.json", "utf-8");
         expenses = JSON.parse(expensesJSON);
     }
+}
+
+function saveExpenses() {
+    const expensesJSON = JSON.stringify(expenses, null, 2);
+    fs.writeFileSync("expenses.json", expensesJSON);
 }
