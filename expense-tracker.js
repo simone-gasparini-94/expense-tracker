aa59a3bc702138b1c811003e8997d597e23eddf2
Simone Gasparini <simone.gasparini.2@gmail.com>
2025-12-31 15:50:06 +0100

    Implement delete and update with ID validity

--- expense-tracker.js
+++ expense-tracker.js
@@ -13,24 +13,69 @@ program
     .command("add")
     .requiredOption("--description <description>")
     .requiredOption("--amount <amount>")
-    .action((options) => {
-        const expenses = loadExpenses();
-        const dateString = new Date().toISOString().slice(0, 10);
-        const array = dateString.split("-");
-        const expense = {
-            id: (expenses.length + 1).toString(),
-            date: dateString,
-            year: array[0],
-            month: array[1],
-            day: array[2],
-            description: options.description,
-            amount: options.amount,
-        };
-        expenses.push(expense);
-        saveExpenses(expenses);
-    });
+    .action(execute);
+
+program
+    .command("delete")
+    .requiredOption("--id <id>")
+    .action(execute);
+
+program
+    .command("update")
+    .requiredOption("--id <id>")
+    .requiredOption("--description <description>")
+    .action(execute);
 
 program.parse();
+
+function execute(options, command) {
+    const actions = {
+        add: addExpense,
+        delete: deleteExpense,
+        update: updateExpense,
+    }
+    const expenses = loadExpenses();
+    actions[command.name()](expenses, options);
+}
+
+function addExpense(expenses, options) {
+    const dateString = new Date().toISOString().slice(0, 10);
+    const array = dateString.split("-");
+    const expense = {
+        id: expenses.length + 1,
+        description: options.description,
+        amount: +options.amount,
+        date: dateString,
+        year: array[0],
+        month: array[1],
+        day: array[2]
+    };
+    expenses.push(expense);
+    saveExpenses(expenses); 
+}
+
+function deleteExpense(expenses, options) {
+    let id = +options.id;
+    checkIdValidity(expenses, id);
+    let newExpenses = [];
+    for (let v of expenses) {
+        if (v.id == id) continue;
+        newExpenses.push(v);
+    }
+    updateIds(newExpenses);
+    saveExpenses(newExpenses);
+}
+
+function updateExpense(expenses, options) {
+    let id = +options.id;
+    checkIdValidity(expenses, id);
+    for (let v of expenses) {
+        if (v.id == id) {
+            v.description = options.description;
+        }
+    }
+    saveExpenses(expenses);
+}
 
 function loadExpenses() {
     if (!fs.existsSync("expenses.json")) {
@@ -44,4 +89,16 @@ function loadExpenses() {
 function saveExpenses(expenses) {
     const expensesJSON = JSON.stringify(expenses, null, 2);
     fs.writeFileSync("expenses.json", expensesJSON);
+}
+
+function updateIds(expenses) {
+    for (let i = 0; i < expenses.length; i++) {
+        expenses[i].id = i + 1;
+    }
+}
+
+function checkIdValidity(expenses, id) {
+    if (id < 1 || id > expenses.length) {
+        throw new Error("Invalid ID");
+    }
 }
