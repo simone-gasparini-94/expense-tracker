4bf41a99287d629b1f1349a6e095e09873101b9e
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-01-03 17:43:40 +0100

    Fix expenses.json path resolution

--- src/file-system.js
+++ src/file-system.js
@@ -1,15 +1,23 @@
 import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const srcPath = fileURLToPath(import.meta.url);
+const srcDir = path.dirname(srcPath);
+const root = path.join(srcDir, "..");
+const jsonPath = path.join(root, "expenses.json");
+
 
 export function loadExpenses() {
-    if (!fs.existsSync("expenses.json")) {
+    if (!fs.existsSync(jsonPath)) {
         return [];
     } else {
-        const expensesJSON = fs.readFileSync("expenses.json", "utf-8");
+        const expensesJSON = fs.readFileSync(jsonPath, "utf-8");
         return JSON.parse(expensesJSON);
     }
 }
 
 export function saveExpenses(expenses) {
     const expensesJSON = JSON.stringify(expenses, null, 2);
-    fs.writeFileSync("expenses.json", expensesJSON);
+    fs.writeFileSync(jsonPath, expensesJSON);
 }
